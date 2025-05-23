9821ec58819ecf8d16b2105fdee6330a5ff0de9c
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 13:54:00 +0300

    enquue coada

--- coada.c
+++ coada.c
@@ -8,7 +8,40 @@ typedef struct Nod
     struct Nod *next;
 } Nod;
 
+typedef struct Coada
+{
+    Nod *inceput;
+    Nod *sfarsit;
+} Coada;
+
+void initCoada(Coada *c)
+{
+    c->inceput = NULL;
+    c->sfarsit = NULL;
+}
+
+void enquue(Coada **c, int valoare)
+{
+    Nod *nou = (Nod *)malloc(sizeof(Nod));
+    nou->valoare = valoare;
+    nou->next = NULL;
+
+    if ((*c)->sfarsit == NULL)
+    {
+        (*c)->inceput = (*c)->sfarsit = nou;
+    }
+    else
+    {
+        (*c)->sfarsit->next = nou;
+        (*c)->sfarsit = nou;
+    }
+}
+
 int main()
 {
+    Coada c;
+    initCoada(&c);
+
+    // enqueue(&c, 5);
     return 0;
 }
