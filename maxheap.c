6093e0175071b0b5f2a9577cb635ce44ae51e64e
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 17:41:21 +0300

    finish max heap

--- maxheap.c
+++ maxheap.c
@@ -50,9 +50,71 @@ void inserare(Heap *h, int valoare)
     h->dimensiune++;
 }
 
+void heapifyDown(Heap *h, int index)
+{
+    int st = 2 * index + 1;
+    int dr = 2 * index + 2;
+    int max = index;
+
+    if (st < h->dimensiune && h->elemente[st] > h->elemente[max])
+    {
+        max = st;
+    }
+    if (dr < h->dimensiune && h->elemente[dr] > h->elemente[max])
+    {
+        max = dr;
+    }
+
+    if (max != index)
+    {
+        swap(&h->elemente[index], &h->elemente[max]);
+        heapifyDown(h, max);
+    }
+}
+
+int extrageMax(Heap *h)
+{
+    if (h->dimensiune <= 0)
+    {
+        printf("Heap-ul este gol.\n");
+        return -1;
+    }
+    int maxim = h->elemente[0];
+    h->elemente[0] = h->elemente[h->dimensiune - 1];
+    h->dimensiune--;
+    heapifyDown(h, 0);
+    return maxim;
+}
+
+void afisareHeap(Heap *h)
+{
+    for (int i = 0; i < h->dimensiune; i++)
+    {
+        printf("%d ", h->elemente[i]);
+    }
+    printf("\n");
+}
+
 int main()
 {
     Heap *heap = creareHeap(20);
+
+    inserare(heap, 10);
+    inserare(heap, 5);
+    inserare(heap, 30);
+    inserare(heap, 20);
+    inserare(heap, 40);
+
+    printf("Heap-ul este:\n");
+    afisareHeap(heap);
+
+    printf("Extragere max: %d\n", extrageMax(heap));
+    printf("Heap-ul după extragere:\n");
+    afisareHeap(heap);
+
+    // nu uita sa eliberezi memoria la final
+    free(heap->elemente);
+    free(heap);
 
     return 0;
 }
