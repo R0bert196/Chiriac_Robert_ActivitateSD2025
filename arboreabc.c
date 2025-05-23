b14078342c739fefe03b052530cf284ab0c630e4
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 11:52:25 +0300

    create insert function

--- arboreabc.c
+++ arboreabc.c
@@ -4,7 +4,43 @@
 
 typedef struct NodABC
 {
-    int valoare;          
-    struct NodABC *stang; 
-    struct NodABC *drept; 
+    int valoare;
+    struct NodABC *stang;
+    struct NodABC *drept;
 } NodABC;
+
+NodABC *creareNod(int valoare)
+{
+    NodABC *nod = (NodABC *)malloc(sizeof(NodABC));
+    nod->valoare = valoare;
+    nod->stang = NULL;
+    nod->drept = NULL;
+    return nod;
+}
+
+void inserare(NodABC **radacina, int valoare)
+{
+    if (*radacina == NULL)
+    {
+        *radacina = creareNod(valoare);
+        return;
+    }
+    if (valoare < (*radacina)->valoare)
+    {
+        // insert la stanga
+        inserare(&(*radacina)->stang, valoare);
+    }
+    else if (valoare > (*radacina)->valoare)
+    {
+        // insert la dreapta
+        inserare(&(*radacina)->drept, valoare);
+    }
+}
+
+int main()
+{
+    NodABC *radacina = NULL;
+
+    // inserare(&radacina, 50);
+    return 0;
+}
