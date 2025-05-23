84085f0b9de3aa98a30953f0a7c943c45bd8eee8
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 09:45:54 +0300

    initial commit for doubled linked listcd

--- dll.c
+++ dll.c
@@ -4,13 +4,30 @@
 
 typedef struct NodDublu
 {
-    int valoare;
+    char *valoare;
     NodDublu *next;
     NodDublu *prev;
 } NodDublu;
 
-int main(int argc, char const *argv[])
+void inserareLaInceput(NodDublu **cap, char *valoare)
 {
+    NodDublu *nou = (NodDublu *)malloc(sizeof(NodDublu));
+    nou->valoare = valoare;
+
+    nou->next = *cap;
+    nou->prev = NULL;
+
+    if (*cap != NULL)
+    {
+        (*cap)->prev = nou;
+    }
+    *cap = nou;
+}
+
+int main()
+{
+
+    NodDublu *cap = NULL;
 
     return 0;
 }
