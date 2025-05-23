72d34406fa389389c2929659c6cee0b050d6c2b5
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 15:12:13 +0300

    create function to create node

--- arboreabc2.c
+++ arboreabc2.c
@@ -8,12 +8,26 @@ typedef struct Farmacie
     char *adresa;
 } Farmacie;
 
-typedef struct Nod
+typedef struct NodABC
 {
     Farmacie farmacie;
-    struct Nod *stanga;
-    struct Nod *dreapta;
-} Nod;
+    struct NodABC *stanga;
+    struct NodABC *dreapta;
+} NodABC;
+
+NodABC *creareNod(char *numeF, char *adresaF)
+{
+    NodABC *nod = (NodABC *)malloc(sizeof(NodABC));
+    nod->farmacie.adresa = (char *)malloc(strlen(adresaF) + 1);
+    nod->farmacie.nume = (char *)malloc(strlen(numeF) + 1);
+
+    strcpy(nod->farmacie.adresa, adresaF);
+    strcpy(nod->farmacie.nume, numeF);
+
+    nod->stanga = nod->dreapta = NULL;
+
+    return nod;
+}
 
 int main()
 {
