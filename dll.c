bda86d78e5657191908c2a984b675b7679e2e722
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 11:20:40 +0300

    add farmacie struct + isnert at end

--- dll.c
+++ dll.c
@@ -2,17 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct
+{
+    char *nume;
+    char *adresa;
+} Farmacie;
+
 typedef struct NodDublu
 {
-    char *valoare;
-    NodDublu *next;
-    NodDublu *prev;
+    Farmacie farmacie;
+    struct NodDublu *next;
+    struct NodDublu *prev;
 } NodDublu;
 
-void inserareLaInceput(NodDublu **cap, char *valoare)
+void inserareLaInceput(NodDublu **cap, char *nume, char *adresa)
 {
     NodDublu *nou = (NodDublu *)malloc(sizeof(NodDublu));
-    nou->valoare = valoare;
+
+    nou->farmacie.nume = (char *)malloc(strlen(nume) + 1);
+    nou->farmacie.adresa = (char *)malloc(strlen(adresa) + 1);
+
+    strcpy(nou->farmacie.nume, nume);
+    strcpy(nou->farmacie.adresa, adresa);
 
     nou->next = *cap;
     nou->prev = NULL;
@@ -24,10 +35,54 @@ void inserareLaInceput(NodDublu **cap, char *valoare)
     *cap = nou;
 }
 
+void inserareLaFinal(NodDublu **cap, char *nume, char *adresa)
+{
+    NodDublu *nou = (NodDublu *)malloc(sizeof(NodDublu));
+
+    nou->farmacie.nume = (char *)malloc(strlen(nume) + 1);
+    nou->farmacie.adresa = (char *)malloc(strlen(adresa) + 1);
+
+    strcpy(nou->farmacie.nume, nume);
+    strcpy(nou->farmacie.adresa, adresa);
+
+    if (*cap == NULL)
+    {
+        nou->next = NULL;
+        nou->prev = NULL;
+        *cap = nou;
+        return;
+    }
+
+    NodDublu *temp = *cap;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+
+    temp->next = nou;
+    nou->prev = temp;
+    nou->next = NULL;
+}
+
+void afisare(NodDublu *cap)
+{
+    while (cap != NULL)
+    {
+        printf("%s  %s", cap->farmacie.adresa, cap->farmacie.nume);
+        printf("\n");
+        cap = cap->next;
+    }
+}
+
 int main()
 {
 
     NodDublu *cap = NULL;
+
+    inserareLaInceput(&cap, "Catena", "Str. Libertatii 12");
+    inserareLaInceput(&cap, "Helpnet", "Str.  12");
+
+    afisare(cap);
 
     return 0;
 }
