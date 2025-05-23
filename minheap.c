2b1ed5a4bd86e902980d263d5eaf61f4d966e776
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 14:06:32 +0300

    minheap initial commit

---
+++ minheap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct
+{
+    int *elemente;
+    int dimensiune;
+    int capacitate;
+} MinHeap;
+
+int main()
+{
+    return 0;
+}
