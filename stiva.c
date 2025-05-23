0a306650f2ae67130ff3592eebada5cce776b77d
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 13:28:50 +0300

    initial for stiva

---
+++ stiva.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct Nod
+{
+    int valoare;
+    struct Nod *next;
+} Nod;
+
+int main()
+{
+    Nod *stiva = NULL;
+    return 0;
+}
