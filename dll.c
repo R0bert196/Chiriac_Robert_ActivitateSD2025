224d7b092e7d6d1d29a1112fcbbfea3e3e7f273c
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 09:21:33 +0300

    initial commit for doubled linked list

---
+++ dll.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct NodDublu
+{
+    int valoare;
+    NodDublu *next;
+    NodDublu *prev;
+} NodDublu;
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
