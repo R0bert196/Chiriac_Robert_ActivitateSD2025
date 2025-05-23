0dc0200744c2677dffaf3699e1a0f828933a0621
R0bert196 <robertchiriac196@yahoo.com>
2025-05-23 14:10:25 +0300

    create heap function

--- minheap.c
+++ minheap.c
@@ -9,7 +9,18 @@ typedef struct
     int capacitate;
 } MinHeap;
 
+MinHeap *creareMinHeap(int capacitate)
+{
+    MinHeap *heap = (MinHeap *)malloc(sizeof(MinHeap));
+    heap->dimensiune = 0;
+    heap->capacitate = capacitate;
+    heap->elemente = (int *)malloc(sizeof(int) * capacitate);
+    return heap;
+}
+
 int main()
 {
+
+    MinHeap *heap = creareHeap(10);
     return 0;
 }
